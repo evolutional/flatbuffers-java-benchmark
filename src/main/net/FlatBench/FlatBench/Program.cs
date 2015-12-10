461671ee62d0910742bf8689c3e8838ff4176cd6
Oli Wilkinson <oli@evolutional.co.uk>
2015-12-10 15:34:31 -0500

    Quick & dirty stats runner for the C# version of the fbbench.fbs code

--- src/main/net/FlatBench/FlatBench/Program.cs
+++ src/main/net/FlatBench/FlatBench/Program.cs
@@ -7,6 +7,42 @@ using FlatBuffers;
 
 namespace FlatBench
 {
+    public struct BenchmarkResult
+    {
+        public string Name { get; set; }
+        public int Iterations { get; set; }
+        public double[] Measurements { get; set; }
+
+        public double Sum
+        {
+            get { return Iterations <= 0 ? Double.NaN : Measurements.Sum(); }
+        }
+
+        public double Mean
+        {
+            get { return Iterations <= 0 ? Double.NaN  : Sum / Iterations; }
+        }
+
+        public double Variance
+        {
+            get
+            {
+                if (Iterations <= 0)
+                {
+                    return Double.NaN;
+                }
+                var mean = Mean;
+                var variance = Measurements.Sum(i => Math.Pow(i - mean, 2));
+                return variance/(Iterations - 1);
+            }
+        }
+
+        public double StandardDeviation
+        {
+            get { return Math.Sqrt(Variance); }
+        }
+    }
+
     public class BenchmarkRunner
     {
         private FlatBench _flatBench = new FlatBench();
@@ -19,54 +55,78 @@ namespace FlatBench
             Position = _buffer.Position;
         }
 
-        public void Run()
+        private BenchmarkResult Measure(string name, Action action, int warmupIterations, int measurements)
         {
+            var result = new BenchmarkResult
+            {
+                Name = name,
+                Iterations = measurements,
+                Measurements = new double[measurements]
+            };
+
+            // Pre collect and finalize
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            // Do warmup for test
+            for (var i = 0; i < warmupIterations; ++i)
+            {
+                action();
+            }
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
             var sw = new Stopwatch();
-            var encodeMeasurements = new TimeSpan[20];
-            var decodeMeasurements = new TimeSpan[20];
-            var useMeasurements = new TimeSpan[20];
-
-            for (var i = 0; i < encodeMeasurements.Length; ++i)
+            for (var i = 0; i < measurements; ++i)
             {
                 sw.Reset();
                 sw.Start();
 
-                _flatBench.Encode(_buffer);
+                action();
+
                 sw.Stop();
-                encodeMeasurements[i] = sw.Elapsed;
+
+                result.Measurements[i] = sw.Elapsed.TotalMilliseconds;
             }
 
-            for (var i = 0; i < decodeMeasurements.Length; ++i)
+            return result;
+        }
+
+        public void Run(int warmupIterations, int measurements)
+        {
+            var encode = Measure("Encode", () => _flatBench.Encode(_buffer), warmupIterations, measurements);
+            var decode = Measure("Decode", () => _flatBench.Decode(_buffer), warmupIterations, measurements);
+            var use = Measure("Traverse", () =>
             {
-                sw.Reset();
-                sw.Start();
-
-                _flatBench.Decode(_buffer);
-                sw.Stop();
-                decodeMeasurements[i] = sw.Elapsed;
-            }
-
-            for (var i = 0; i < useMeasurements.Length; ++i)
-            {
-                sw.Reset();
-                sw.Start();
-
                 _buffer.Position = Position;
                 _flatBench.Use(_buffer);
-                sw.Stop();
-                useMeasurements[i] = sw.Elapsed;
-            }
+            }, warmupIterations, measurements);
+
+            var e = new TimeSpan(0, 0, 0, 0, (int)(encode.Mean * 1000000));
+            var d = new TimeSpan(0, 0, 0, 0, (int)(decode.Mean * 1000000));
+            var u = new TimeSpan(0, 0, 0, 0, (int)(use.Mean * 1000000));
+            var du = d + u;
+
+            Console.WriteLine("{0,-25}\t{1, 10} {2, 6}  Unit {3,10}", "Name", "Mean", "StdD", "1M/sec");
+            Console.WriteLine("{0, -25}\t{1, 10:0.000} {2,6:0.000} ms/op {3,10:0.0}", encode.Name, encode.Mean, encode.StandardDeviation, e.TotalSeconds);
+            Console.WriteLine("{0, -25}\t{1, 10:0.000} {2,6:0.000} ms/op {3,10:0.0}", decode.Name, decode.Mean, decode.StandardDeviation, d.TotalSeconds);
+            Console.WriteLine("{0, -25}\t{1, 10:0.000} {2,6:0.000} ms/op {3,10:0.0}", use.Name, use.Mean, use.StandardDeviation, u.TotalSeconds);
         }
 
     }
 
     class Program
     {
-
         static void Main(string[] args)
         {
             var runner = new BenchmarkRunner();
-            runner.Run();
+            runner.Run(10, 50);
+
+            Console.WriteLine("\nPress a key...");
+            Console.ReadKey();
         }
     }
 }
